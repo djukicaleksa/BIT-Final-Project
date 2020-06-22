8f5af2451b064ef6e4c99bbf2168f73a9ddc760a
BIT Student <classroom@bgit.rs>
2020-06-22 11:44:36 +0200

    Creating axios instance and service for data fetching

---
+++ react-interview-reports/src/services/servicePeople.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { baseAPI } from '../shared/baseAPI'
+
+// http://localhost:3333/api/companies
+// http://localhost:3333/api/candidates
+// http://localhost:3333/api/reports
+// http://localhost:3333/api/users
+
+class ServicePeople {
+
+    getCompanies() {
+        return baseAPI.get('companies')
+            .then(response => response.data)
+            .catch(error => console.log(error))
+    }
+
+    getCandidates() {
+        return baseAPI.get('candidates')
+            .then(response => response.data)
+            .catch(error => console.log(error))
+    }
+
+    getReports() {
+        return baseAPI.get('reports')
+            .then(response => response.data)
+            .catch(error => console.log(error))
+    }
+
+    postReport(reportObject) {
+        return baseAPI.post('reports', reportObject)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }
+}
