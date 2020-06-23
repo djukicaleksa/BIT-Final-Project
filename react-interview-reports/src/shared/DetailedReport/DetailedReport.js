c40ccf54b8ce4a5ab9cf161fd77fbadd784eb40e
BIT Student <classroom@bgit.rs>
2020-06-23 15:12:17 +0200

    finished modal implementation

--- react-interview-reports/src/shared/DetailedReport/DetailedReport.js
+++ react-interview-reports/src/shared/DetailedReport/DetailedReport.js
@@ -16,22 +16,28 @@ export const DetailedReport = (props) => {
         contentLabel="Example Modal"
     >
         {/*Modal html goes below */}
-        <div className='modalWrapper'>
+        <div className={style.modalWrapper}>
             <h2>
-                {props.candidateName}
+                {props.report.candidateName}
             </h2>
             <hr></hr>
-            <div>
-                <label className={style.textLabel}>Company :<p className={style.reportText}>{props.report.companyName}</p></label>
-                <label className={style.textLabel}>Interview Date :<p className={style.reportText}>{convertDate(props.report.interviewDate)}</p></label>
-                <label className={style.textLabel}>Phase :<p className={style.reportText}>{props.report.phase}</p></label>
-                <label className={style.textLabel}>Status :<p className={style.reportText}>{props.report.status}</p></label>
-            </div>
-            <div>
-                <label>Company :<p>DescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDescDesc</p></label>
+            <div className={style.detailsWrapper}>
+                <div>
+                    <label className={style.textLabel}>Company :<p className={style.reportText}>{props.report.companyName}</p></label>
+                    <label className={style.textLabel}>Interview Date :<p className={style.reportText}>{convertDate(props.report.interviewDate)}</p></label>
+                    <label className={style.textLabel}>Phase :<p className={style.reportText}>{props.report.phase}</p></label>
+                    <label className={style.textLabel}>Status :<p className={style.reportText}>{props.report.status}</p></label>
+                </div>
+                <div className={style.descriptionWrapper}>
+                    <label className={style.description}>Description :<p className={style.reportText}>Nulla lacinia nibh erat, et aliquam eros bibendum eu. 
+                    Nam pellentesque lobortis placerat. Cras nec elit ipsum. Ut venenatis turpis quis tortor efficitur sagittis. Donec ac lorem dictum, 
+                    posuere nulla vel, egestas mi. Sed placerat, odio eget vestibulum malesuada, lorem orci ultricies ex, a congue nisl felis non est. 
+                    Cras ac sollicitudin libero. Sed volutpat condimentum velit et condimentum. Suspendisse convallis justo eu mi ornare, in posuere eros
+                     malesuada. Sed egestas accumsan sagittis. Maecenas eget dapibus tortor, id rutrum magna. Nunc sed porttitor massa. Suspendisse nec 
+                     sodales neque, id varius mi.</p></label>
+                </div>
             </div>
         </div>
 
-        {/*Modal html goes above */}
     </Modal>)
 }
--- react-interview-reports/src/shared/constants.js
+++ react-interview-reports/src/shared/constants.js
@@ -8,6 +8,8 @@ export const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
+        width: '50vw'
+
     }
 };
 
