41ab52effe8a3307780789d8562c54feab5e2ba0
BIT Student <classroom@bgit.rs>
2020-06-24 11:33:56 +0200

    Creating ErrorBoundary component, but still not sure how it works(does it work)

--- react-interview-reports/src/App.js
+++ react-interview-reports/src/App.js
@@ -7,17 +7,20 @@ import { Switch, Route } from 'react-router-dom';
 import { LoginPage } from './components/Admin/LoginPage/LoginPage';
 import { ReportPage } from './components/Admin/ReportsPage/ReportsPage';
 import { CreateReportPage } from './components/Admin/CreateReportPage/CreateReportPage';
+import { ErrorBoundary } from './shared/ErrorBoundary';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info/:id' component={InfoPage} />
-        <Route exact path='/admin' component={LoginPage} />
-        <Route exact path='/admin/reports' component={ReportPage} />
-        <Route exact path='/admin/reports/createreport' component={CreateReportPage} />
-      </Switch>
+      <ErrorBoundary>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/info/:id' component={InfoPage} />
+          <Route exact path='/admin' component={LoginPage} />
+          <Route exact path='/admin/reports' component={ReportPage} />
+          <Route exact path='/admin/reports/createreport' component={CreateReportPage} />
+        </Switch>
+      </ErrorBoundary>
     </div>
   );
 }
